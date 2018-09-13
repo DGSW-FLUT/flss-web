90fc0e41f81f48875163ae36448ec16022539a90
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-09-13 20:22:33 +0900

    [ADD] Vuex + Vue-Router 활용 로그인 로그아웃 스캐폴딩

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="logout()">Logout</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      this.$store.commit("setAuthenticated", false);
+      this.$router.push({ path: "/login" });
+    }
   }
 };
 </script>
