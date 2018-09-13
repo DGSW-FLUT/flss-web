8392af039c4dfcd36a42ab16ab4be4a70b727fc7
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-09-13 19:41:31 +0900

    [ADD] 작업을 위한 스캐폴딩

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Vuevent from "vuevent";
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuevent);
 
 new Vue({
   router,
