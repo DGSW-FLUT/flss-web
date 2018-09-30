b7d22c186cb030637ad9b5684d147570be137f3a
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-09-30 23:53:23 +0900

    [ADD] Reactive data import with chart

--- src/components/ExampleChart.vue
+++ src/components/ExampleChart.vue
@@ -1,17 +1,18 @@
 <script>
-import { Bar } from "vue-chartjs";
+import { Doughnut } from "vue-chartjs";
 
 export default {
   name: "example-chart",
-  extends: Bar,
+  extends: Doughnut,
   mounted() {
     this.renderChart({
       labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
       datasets: [
         {
           label: "Github Commits",
-          backgroundColor: "#F87979",
-          data: [40, 20, 12, 39, 10, 35, 40, 20, 12, 39, 10, 35]
+          backgroundColor: ["red", "blue", "green"],
+          data: [40, 20, 12, 39, 10, 35, 40, 20, 12, 39, 10, 35],
+          devicepixelRatio: "16by9"
         }
       ]
     });
--- src/views/class/Evaluation.vue
+++ src/views/class/Evaluation.vue
@@ -2,24 +2,35 @@
   <div id="evaluation">
     <b-container>
       <div class="mt-5"></div>
-      <b-card no-body class="p-5 col-md-4">
-        <example-chart />
-      </b-card>
+      <b-row>
+        <b-card no-body class="p-5 col-md-4 m-3">
+          <bar-chart :data="chartData" />
+        </b-card>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-import ExampleChart from "@/components/ExampleChart"
+import BarChart from "@/components/BarChart"
 
 export default {
   name: "evaluation",
   components: {
-    ExampleChart
+    BarChart
   },
   data() {
     return {
-
+      chartData: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [
+          {
+            label: "Github Commits",
+            backgroundColor: "#F87979",
+            data: [40, 20, 12, 39, 10, 35, 40, 20, 12, 39, 10, 35]
+          }
+        ]
+      }
     }
   }
 };
