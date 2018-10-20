ace673bd3eac0a8bf77455953ce1fe6f384eb4a8
tbvjaos510 <tbvjaos510@naver.com>
2018-10-20 19:48:09 +0900

    [ADD] PortfolioItem.vue에 주석 추가 10번째 줄 주석 참고바람

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -7,8 +7,9 @@
             @click="add(i)">
             <h4 slot="header">{{ portfolio.Title }}</h4>
             <div class="border-bottom">
-                <font-awesome-icon class="py-2" fas icon="paperclip" size="2x" data-html2canvas-ignore="true" />
-                {{ portfolio.Name }}
+              <!-- data-html2canvas-ignore="true"로 하면 pdf로 변환시 해당 element는 보이지 않는다. 체크박스에 추가해주길 바람-->
+              <font-awesome-icon class="py-2" fas icon="paperclip" size="2x" data-html2canvas-ignore="true" />
+              {{ portfolio.Name }}
             </div>
             <b-card-body>
             <p class="card-text">
