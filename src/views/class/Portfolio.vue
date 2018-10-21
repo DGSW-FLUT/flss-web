2ef76cd6e7f067d91ec9e8e12b0e3aa0f5834386
PTHy <30455428+PTHy@users.noreply.github.com>
2018-10-22 00:21:18 +0900

    [FIX] 포트폴리오 교사 의견, 제목 달 수잇게 수정

--- src/views/class/Portfolio.vue
+++ src/views/class/Portfolio.vue
@@ -3,16 +3,29 @@
         <b-container>
             <h3 class="mt-5 mb-5">포트폴리오 만들기</h3>
             <b-input-group class="col-md-5 mb-5">
-                <b-form-input placeholder="Enter search student" v-model="studentName"></b-form-input>
+                <b-form-input placeholder="찾을 학생 이름" v-model="studentName"></b-form-input>
                 <b-button slot="append" variant="success" @click="search">검색</b-button>
             </b-input-group>
-            <b-button style="float:right" v-if="portfolios.length" variant="primary" @click="createPDF">포트폴리오 만들기</b-button>
+            <div v-if="portfolios.length">
+              <b-row>
+                <b-col lg="7">
+                  포트폴리오 제목 <b-form-input class="mt-3" placeholder="포트폴리오 제목을 입력해주세요" v-model="portfolioTitle"></b-form-input> <br>
+                  교사의견 <b-form-textarea
+                            v-model="teacherOpinion"
+                            class="mb-3 mt-3"
+                            style="resize:none"
+                            placeholder="교사 의견을 입력해주세요"
+                            rows="3" />
+                  <b-button style="float:right" variant="primary" @click="createPDF">포트폴리오 만들기</b-button>
+                </b-col>
+              </b-row>
+            </div>
             <span class="aligncenter mt-5" v-if="!portfolios.length"> 
                 <font-awesome-icon class="py-2 ml-5 mr-5 " fas icon="exclamation-circle" size="10x"/><br>
                 <span style="font-size:2em">등록된 게시물이 없습니다.</span>
               </span>
             <div id="portfolioitems">
-              <h3 class="mt-5 mb-5" v-if="portfolios.length">{{ studentName }} 포트폴리오</h3>
+              <h3 class="mt-5 mb-5" v-if="portfolios.length">{{ selectedStudentName }} 포트폴리오</h3>
               <portfolio-item v-for="(portfolio,i) in portfolios" :key="i" :portfolio="portfolio" :i="i"></portfolio-item>
             </div>
         </b-container>
@@ -30,12 +43,21 @@ export default {
     return {
       portfolios: [],
       selected: [],
-      studentName: ""
+      selectedStudentName: "",
+      studentName: "",
+      teacherOpinion: "",
+      portfolioTitle: ""
     };
+  },
+  computed: {
+    isValidInput() {
+      return this.portfolioTitle && this.teacherOpinion && (this.selected.length !== 0);
+    }
   },
   methods: {
     search() {
       this.selected = [];
+      this.selectedStudentName = this.studentName;
       this.$http
         .get(
           "http://flss.kr/api/data/getPostByName?cid=" +
@@ -52,6 +74,10 @@ export default {
         });
     },
     createPDF() {
+      if(!this.isValidInput) {
+        alert("입력하지 않은 부분이 있습니다. 확인해주세요");
+        return;
+      }
       html2canvas(document.getElementById("portfolioitems"))
         .then((canvas) => {
           var imgData = canvas.toDataURL('image/png');           
@@ -72,7 +98,8 @@ export default {
             heightLeft -= pageHeight;
           }
           // 파일 저장
-          doc.save(new Date().toJSON().split('T')[0] + '_' + this.studentName + '학생_포트폴리오' + '.pdf');
+          // doc.save(new Date().toJSON().split('T')[0] + '_' + this.studentName + '학생_포트폴리오' + '.pdf');
+          doc.save(this.portfolioTitle + '.pdf');
         })
     }
   },
